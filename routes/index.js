65bb0f5ea896903b96977053e7ccce419411d294
fredrikburmester <fredrik.burmester@gmail.com>
2020-11-07 21:02:52 +0100

    Update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/:name', function(req, res, next) {
+  res.render('index', { title: 'Home', 'album': req.params.name});
 });
 
 module.exports = router;
