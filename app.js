6c6061dacc28db6f31252842acbb769d20e6ff99
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-03-07 15:37:01 +0000

    added preview image to links

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var cookieParser = require('cookie-parser');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var createError = require('http-errors');
 var express = require('express');
 var logger = require('morgan');
@@ -19,6 +18,11 @@ if(process.env.ENV == 'production') {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.set('port', process.env.PORT || 8000);
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +30,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
